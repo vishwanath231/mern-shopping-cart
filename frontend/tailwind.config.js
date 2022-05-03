feb91b375f899094b000f271218bcc8f444d9682
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-05-03 16:23:14 +0530

    Tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Sen', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
