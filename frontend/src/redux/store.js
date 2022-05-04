80ff0a987fd0bec1e301e5c24b2782c546a47300
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-05-04 22:08:35 +0530

    Redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,13 +3,22 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducers } from './reducers';
 
+
 const middleware = [thunk];
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 
 const initialState = {
     cart: {
-        cartItems: cartItemsFromStorage
+        cartItems: cartItemsFromStorage,
+        shippingAddress: shippingAddressFromStorage
+    },
+    userLogin: {
+        userInfo: userInfoFromStorage
     }
 }
 
