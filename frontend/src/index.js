5d16b7e2e76e75563fa7b28a361cef40d283bcf8
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-05-04 22:20:16 +0530

    React index app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import './bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
