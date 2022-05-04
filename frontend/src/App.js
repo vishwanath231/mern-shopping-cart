8bbc67277f72deec7ed99a99f87184fe6decc072
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-05-04 22:18:20 +0530

    React app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import { BrowserRouter, Route, Routes,  } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Navbar />
-            <div className=' px-2 md:px-0 max-w-screen-lg mx-auto'>
+            <Header />
+            <Container>
                 <Routes>
                     <Route path='/' element={<HomeScreen /> } />
                     <Route path='/product/:id' element={<ProductScreen /> } />
                     <Route path='/cart' element={<CartScreen /> } />
+                    <Route path='/cart/:id' element={<CartScreen /> } />
+                    <Route path='/login' element={<LoginScreen />} />
+                    <Route path='/register' element={<RegisterScreen /> } />
+                    <Route path='/login/shipping' element={<ShippingScreen /> } />
+                    <Route path='/register/shipping' element={<ShippingScreen /> } />
+                    <Route path='/payment' element={<PaymentScreen /> } />
+                    <Route path='/placeorder' element={<PlaceOrderScreen /> } />
                 </Routes>
-            </div>
+            </Container>
         </BrowserRouter>
     )
 }
