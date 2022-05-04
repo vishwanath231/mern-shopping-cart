f494e3e881b6e719be192d9088dc51dd141c30a9
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-05-04 22:09:57 +0530

    Cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -21,26 +21,32 @@ const CartScreen = ({ cart, addToCart, removeFromCart }) => {
     }
 
 
+    const checkoutHandler = () => {
+    
+        navigate('/login?redirect=shipping')   
+      }
+
+
     const { cartItems } = cart;
 
 
     return (
         <>
 
-            <div className='uppercase text-2xl my-6 tracking-wider font-medium'>shopping cart</div>
+            <div className='uppercase text-2xl my-6 text-black tracking-wider font-medium'>shopping cart</div>
 
             <div className='flex justify-between flex-col md:flex-row' >
                 <div className='p-4 w-full'>
                     {
                         cartItems.map(item => (
                             <div key={item.product} className='mb-4'>
-                                <div className='flex justify-between '>
+                                <div className='grid gap-3 grid-cols-5'>
                                     <img src={item.image} alt={item.name} className='text-center w-20' />
                                     <Link to={`/product/${item.product}`} className='underline text-slate-500'>{item.name}</Link>
                                     
-                                    <div className='text-slate-500'>₹{item.price}</div>
+                                    <div className='text-slate-500 text-center'>₹{item.price}</div>
                                     
-                                    <div className='text-slate-500'>
+                                    <div className='text-slate-500 text-center'>
                                         <select value={item.qty} className='' onChange={(e) => addToCart(item.product ,Number(e.target.value))}>
                                             {
                                                 [...Array(item.countInStock).keys()].map((x) => (
@@ -50,7 +56,7 @@ const CartScreen = ({ cart, addToCart, removeFromCart }) => {
                                         </select>
                                     </div>
                                     
-                                    <div className=''>
+                                    <div className='text-center'>
                                         <button className='text-orange-800' onClick={() => removeFromCartHandler(item.product)}><i className='fas fa-trash'></i></button>
                                     </div>
                                 </div>
@@ -58,7 +64,7 @@ const CartScreen = ({ cart, addToCart, removeFromCart }) => {
                         ))
                     }
                 </div>
-                <div className='w-1/2'>
+                <div className='w-full md:w-1/2 my-0 mx-auto'>
                     <div className='border-2'>
                         <div className='p-3 border-b-2'>
                             <h2 className='text-2xl pb-3'>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0) }) items</h2>
@@ -66,6 +72,7 @@ const CartScreen = ({ cart, addToCart, removeFromCart }) => {
                         </div>
                         <div className='p-3'>
                             <button
+                            onClick={checkoutHandler}
                             className='uppercase  text-sm tracking-wide bg-black w-full p-3 text-white'
                             >Proceed To Checkout</button>
                         </div>
