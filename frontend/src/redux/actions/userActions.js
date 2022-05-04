8866dd1e389b815fad5ee58f94eb3424bc35463c
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-05-04 22:05:21 +0530

    User actions

---
+++ frontend/src/redux/actions/userActions.js
@@ -0,0 +1,93 @@
+import {
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
+    USER_LOGOUT
+} from '../constants/userConstants';
+import axios from 'axios'
+
+
+export const login = (loginData) => async (dispatch) => {
+
+    try {
+        
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    
+        const { data } = await axios.post(`http://localhost:5000/api/users/login`, loginData, config)
+    
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (err) {
+     
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+
+
+
+export const register = (registerData) => async (dispatch) => {
+
+    try {
+        
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    
+        const { data } = await axios.post(`http://localhost:5000/api/users/register`, registerData, config)
+    
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (err) {
+     
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+
+
+export const logout = () => (dispatch) => {
+
+    localStorage.removeItem('userInfo')
+
+    dispatch({type: USER_LOGOUT })
+
+}
