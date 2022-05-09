54785a6cbfc2c65dbe3920afff5278e5f2c61185
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-05-09 19:48:45 +0530

    User reducers

--- frontend/src/redux/reducers/userReducers.js
+++ frontend/src/redux/reducers/userReducers.js
@@ -5,7 +5,14 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
-    USER_LOGOUT
+    USER_LOGOUT,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_RESET,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
 } from '../constants/userConstants';
 
 
@@ -59,6 +66,66 @@ export const userRegisterReducer = (state = { }, { type, payload }) => {
         case USER_LOGOUT:
             return { }
 
+        default:
+            return state;
+    }
+}
+
+
+
+
+export const userDetailsReducer = (state = { user: {} }, {type, payload}) => {
+     
+    switch (type) {
+        case USER_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+
+        case USER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                user: payload
+            }
+
+        case USER_DETAILS_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+        case USER_DETAILS_RESET: 
+            return {
+                user: {}
+            }
+        default:
+            return state;
+    }
+}
+
+
+
+export const userUpdateProfileReducer = (state = { user: {} }, {type, payload}) => {
+     
+    switch (type) {
+        case USER_UPDATE_PROFILE_REQUEST:
+            return {
+                loading: true,
+            }
+
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return {
+                loading: false,
+                success: true,
+                user: payload
+            }
+
+        case USER_UPDATE_PROFILE_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+        
         default:
             return state;
     }
