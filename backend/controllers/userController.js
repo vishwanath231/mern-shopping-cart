709a7f66218cb7fab1e5bff763bf069d5ac570eb
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-05-09 20:10:57 +0530

    User controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -100,11 +100,84 @@ const register = asyncHandler(async (req, res) => {
             }
         })
     }
-
 })
+
+
+const getUserProfile = asyncHandler(async (req, res) => {
+
+    const user = await User.findById(req.user._id)
+    if (!user) {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+    res.json({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin
+    })
+})
+
+
+
+/**
+ * @description  Update user profile
+ * @route        PUT /api/users/profile
+ * @access       Public
+ */
+ const updateUserProfile = asyncHandler(async (req, res) => {
+    
+    const user = await User.findById(req.user.id);
+    if (!user) {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    if (!salt) {
+        res.status(400)
+        throw new Error("something went wrong with bcrypt"); 
+    }
+
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    if (!hashPassword) {
+        res.status(400)
+        throw new Error("something went wrong with hashing password");
+    }
+
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+        user.password = hashPassword || user.password
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(201).json({ 
+        msg: "Updated successfull!",
+        success:  true,
+        token: generateToken(updatedUser._id),
+        user: {
+            id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        }
+    })
+
+});
+
+
+
+
+
+
 
 
 export {
     login,
-    register
+    register,
+    getUserProfile,
+    updateUserProfile
 }
