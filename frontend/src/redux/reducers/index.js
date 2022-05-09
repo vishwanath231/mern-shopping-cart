0baeb54017f0c62ca806decb97ca2c465c0f11c3
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-05-09 19:46:00 +0530

    Reducer index

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,12 +1,36 @@
 import { combineReducers } from 'redux';
 import { cartReducer } from './cartReducers';
 import { productDetailsReducer, productListReducer } from './ProductReducers';
-import { userLoginReducer, userRegisterReducer } from './userReducers';
+
+import { 
+    userLoginReducer, 
+    userRegisterReducer, 
+    userDetailsReducer, 
+    userUpdateProfileReducer 
+} from './userReducers';
+
+import { 
+    orderCreateReducer, 
+    orderDetailsReducer, 
+    orderListMyPayReducer,
+    orderPayReducer,
+    orderDeliverReducer 
+} from './orderReducers';
 
 export const reducers = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+
     cart: cartReducer,
+
     userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
+
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderListMy: orderListMyPayReducer,
+    orderPay: orderPayReducer,
+    orderDeliver: orderDeliverReducer
 });
